86fa17bf25815153ba8e90ccf2e804a503ae56f1
Arwan Ahmad Khoiruddin <arwan.khoiruddin@gmail.com>
2024-02-23 14:45:14 +0700

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,3 @@
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
-sudo apt-get install -Y python3.11 python3.11-full python3-pip
+sudo apt-get install -y python3.11 python3.11-full python3-pip
